e8ef4e99b6e564890cc5731070fcec3c16546417
Lam <daibanglam@gmail.com>
2020-04-04 11:09:05 +0700

    Optimize loading state hook

--- src/common/hooks/useLoadingState.js
+++ src/common/hooks/useLoadingState.js
@@ -4,6 +4,8 @@ function init(options) {
   const initialState = {
     data: null,
     error: undefined,
+    // loading flag is set to true if async function
+    // will be called automatically after component is rendered
     loading: !options.defer,
   };
   return initialState;
@@ -32,7 +34,8 @@ function reducer(state, action) {
  */
 export default function useLoadingState(fn, options = {}) {
   const opt = {
-    // don't run async action on load
+    // specify function should be called automatically after
+    // component is rendered
     defer: false,
     // throw exception when async function is rejected
     exception: false,
@@ -48,7 +51,12 @@ export default function useLoadingState(fn, options = {}) {
 
   async function load(...params) {
     try {
-      dispatch({ type: 'start' });
+      // dispatch start loading event if function was
+      // not called automatically or it's not the first run
+      if (opt.defer || state.data !== null) {
+        dispatch({ type: 'start' });
+      }
+
       const res = await fn(...params);
       dispatch({ type: 'load-finish', data: res });
       return res;
