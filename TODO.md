1ce9c99aa694d2bdfbee80e82d8ce8d8d8d9f1c8
Lam <daibanglam@gmail.com>
2020-04-09 17:26:42 +0700

    Host assets in cdn (Cloudfront)

--- TODO.md
+++ TODO.md
@@ -1,23 +1,22 @@
 # TODO Mon 6/4 => 12/4
 ```yml
+i18n
+  # enable catching missing translations
+  enable runtime language switching
+  test all features
+  resolve missing translations
 # user management
 # handling error
-deployment
-  deploy script
+# deployment
+  # configure bitbucket-pipeline.yml
+  # deploy script
     # setup publicPath for the build
     # test the build
     # create docker compose file
     # proxy api request to api service
     # create docker compose + init db
     # push docker image to docker hub
-    host assets in CDN (cloudfront)
-      can not create Cloudfront distribution due to service limit
-  configure bitbucket pipeline
-i18n
-  enable catching missing translations
-  enable runtime language switching
-  test all features
-  resolve missing translations
+    # host assets in CDN (cloudfront)
 ```
 
 
@@ -27,9 +26,13 @@ i18n
 # user management
 # handling error
 deployment
-integrate js error reporting (usersnap + slack notification)
 i18n
+error reporting (sentry + slack notification)
 upgrade packages to latest version
+logging
+  https://www.scalyr.com/blog/getting-started-react-logging/
+  https://www.loggly.com/blog/best-practices-for-client-side-logging-and-error-handling-in-react/
 integrate typescript
+unit test
 ```
 
--- scripts/deploy/run.sh
+++ scripts/deploy/run.sh
@@ -1,37 +1,27 @@
 #!/bin/bash
-# export CDN_BASE_URL=/
-# export AWS_ACCESS_KEY_ID=AKIA3GMRGG2JF3GB26WD
-# export AWS_SECRET_ACCESS_KEY=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
-# export AWS_DEFAULT_REGION=ap-southeast-1
-# export AWS_BUCKET=rest-boilerplate-assets
-export API_BASE_URL=http://api:3000
-export DOCKER_HUB_USER=lamhq
-export DOCKER_HUB_PASSWORD=69b94abb-f5e1-4288-abb7-973c6a53aef6
-export DOCKER_HUB_REPO=rest-web
-export DOCKER_TAG=latest
 export NODE_ENV=production
 
 # cd to script directory
 cd "$(dirname "$0")"
 
 # build react app
-# yarn build
+yarn build
 
 # install AWS CLI, https://docs.aws.amazon.com/cli/latest/userguide/install-bundle.html#install-bundle-user
-# curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
-# unzip awscli-bundle.zip
-# ./awscli-bundle/install -b ~/bin/aws
-# export PATH=~/bin:$PATH
+curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "awscli-bundle.zip"
+unzip awscli-bundle.zip
+./awscli-bundle/install -b ~/bin/aws
+export PATH=~/bin:$PATH
 
 # empty s3 bucket and upload new assets
-# aws s3 rm s3://${AWS_BUCKET} --recursive
-# aws s3 cp build s3://${AWS_BUCKET}/ --recursive --exclude "*.html"
+aws s3 rm s3://${AWS_BUCKET}/assets --recursive
+aws s3 cp dist s3://${AWS_BUCKET}/assets --recursive --exclude "*.html"
 
-# # build docker image
+# build docker image
 sed "s@{API_BASE_URL}@${API_BASE_URL}@g;" nginx.conf.tpl > nginx.conf
-export IMAGE=${DOCKER_HUB_USER}/${DOCKER_HUB_REPO}:${DOCKER_TAG}
+export IMAGE=${DOCKER_HUB_USER}/${DOCKER_HUB_REPO}:$(date +"%y%m%d.%H%M").${BITBUCKET_COMMIT:0:6}
 docker build -t ${IMAGE} .
 
-# # push image to docker hub
+# push image to docker hub
 echo ${DOCKER_HUB_PASSWORD} | docker login -u ${DOCKER_HUB_USER} --password-stdin
 docker push ${IMAGE}
