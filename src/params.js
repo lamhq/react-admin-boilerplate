6c85700a22b4efee289f85af2a0bea073dea5f0e
Lam <daibanglam@gmail.com>
2020-04-21 10:44:47 +0700

    fix: version is not displayed

--- src/params.js
+++ src/params.js
@@ -1,5 +1,5 @@
 export const appName = 'React Boilerplate';
-export const appVersion = process.env.npm_package_version;
+export const appVersion = process.env.VERSION;
 export const environment = process.env.ENVIRONMENT;
 export const sentryDsn = process.env.SENTRY_DSN;
 export const apiBaseUrl = '/api/v1';
